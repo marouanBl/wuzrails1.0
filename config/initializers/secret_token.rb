c82a4ba3bcb2c07f6225c6fbcfa8ec3a4cd339a1
marwen <marwen.belghith@gmail.com>
2013-02-20 08:28:23 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wuzrails10::Application.config.secret_token = '9d1adcd239e340072a6d61d1b97ac0f0016f4d51d17b0026ae2263e84d20e555046a8e61529060bed44e70848ad13b52c5e272a447783e9e9946a2182c42577a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Wuzrails10::Application.config.session_store :cookie_store, key: '_wuzrails1.0_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Wuzrails10::Application.config.session_store :active_record_store
